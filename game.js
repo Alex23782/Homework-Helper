286b1394648374d5823e0b6845aaf87d156d4c17
Alex23782 <alexvivot25@gmail.com>
2025-05-07 12:02:16 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,29 @@
 let score = 0;
+let timeLeft = 10;
+let timerStarted = false;
 
 function increaseScore() {
-  score++;
-  document.getElementById("score").innerText = "Score: " + score;
+  if (!timerStarted) {
+    startTimer();
+    timerStarted = true;
+  }
+
+  if (timeLeft > 0) {
+    score++;
+    document.getElementById("score").innerText = "Score: " + score;
+  }
 }
+
+function startTimer() {
+  const timerElement = document.getElementById("timer");
+  const countdown = setInterval(() => {
+    timeLeft--;
+    timerElement.innerText = "Time left: " + timeLeft + "s";
+
+    if (timeLeft <= 0) {
+      clearInterval(countdown);
+      alert("Time's up! Final score: " + score);
+    }
+  }, 1000);
+}
+
